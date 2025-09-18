5601846d7deae974af5274f1a0ad03aca3d4e5cb
Paweł Opala <p.opala1986@gmail.com>
2025-09-18 10:18:18 +0200

    Initial commit

---
+++ src/main/java/pl/onewaytickettravel/app/OnewayTicketTravelApplication.java
@@ -0,0 +1,13 @@
+package pl.onewaytickettravel.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnewayTicketTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnewayTicketTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/onewaytickettravel/app/OnewayTicketTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.onewaytickettravel.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnewayTicketTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
