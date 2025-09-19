43184f14dace8343a88d81a9b583309efe5baddf
Paweł Opala <p.opala1986@gmail.com>
2025-09-19 10:46:59 +0200

    chore(security): allow public access to /search endpoint

--- src/main/java/pl/onewaytickettravel/app/configuration/security/SecurityConfig.java
+++ src/main/java/pl/onewaytickettravel/app/configuration/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                                 "/js/**",
                                 "/images/**",
                                 "/ws/**",
+                                "/search",
                                 "/csrf-token"
                         ).permitAll()
                         .anyRequest().authenticated()
