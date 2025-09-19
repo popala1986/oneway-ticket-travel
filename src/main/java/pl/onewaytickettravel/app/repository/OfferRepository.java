a525479d4bfea3d7eef7a2338542040eee420be8
Paweł Opala <p.opala1986@gmail.com>
2025-09-19 10:37:13 +0200

    feat(repository): enable specification support in OfferRepository

--- src/main/java/pl/onewaytickettravel/app/repository/OfferRepository.java
+++ src/main/java/pl/onewaytickettravel/app/repository/OfferRepository.java
@@ -1,9 +1,10 @@
 package pl.onewaytickettravel.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import pl.onewaytickettravel.app.entities.Offer;
 
 @Repository
-public interface OfferRepository extends JpaRepository<Offer, Long> {
+public interface OfferRepository extends JpaRepository<Offer, Long>, JpaSpecificationExecutor<Offer> {
 }
