513291cabf41e08e4929ece59f8bdd6314daaed8
Paweł Opala <p.opala1986@gmail.com>
2025-09-22 15:12:51 +0200

    chore: added City table definition to schema.sql and sample data to data.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,31 @@
--- Tworzenie bazy danych
+---- Tworzenie bazy danych
+--CREATE DATABASE IF NOT EXISTS onewaytickettravel;
+--USE onewaytickettravel;
+--
+---- Tabela dla Kontynentów
+--CREATE TABLE IF NOT EXISTS continents (
+--    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    name VARCHAR(20) NOT NULL UNIQUE
+--) ENGINE=InnoDB;
+--
+---- Tabela dla Krajów
+--CREATE TABLE IF NOT EXISTS countries (
+--    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    name VARCHAR(20) NOT NULL UNIQUE,
+--    continent_id BIGINT,
+--    FOREIGN KEY (continent_id) REFERENCES continents(id)
+--) ENGINE=InnoDB;
+--
+---- Tabela dla Ofert
+--CREATE TABLE IF NOT EXISTS offers (
+--    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    name VARCHAR(150) NOT NULL,
+--    price DECIMAL(12, 2) NOT NULL,
+--    continent_id BIGINT NOT NULL,
+--    country_id BIGINT,
+--    FOREIGN KEY (continent_id) REFERENCES continents(id),
+--    FOREIGN KEY (country_id) REFERENCES countries(id)
+--) ENGINE=InnoDB;
 CREATE DATABASE IF NOT EXISTS onewaytickettravel;
 USE onewaytickettravel;
 
@@ -16,13 +43,22 @@ CREATE TABLE IF NOT EXISTS countries (
     FOREIGN KEY (continent_id) REFERENCES continents(id)
 ) ENGINE=InnoDB;
 
--- Tabela dla Ofert
+-- Tabela dla Miast
+CREATE TABLE IF NOT EXISTS cities (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL UNIQUE,
+    country_id BIGINT,
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS offers (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     price DECIMAL(12, 2) NOT NULL,
     continent_id BIGINT NOT NULL,
     country_id BIGINT,
+    city_id BIGINT,
     FOREIGN KEY (continent_id) REFERENCES continents(id),
-    FOREIGN KEY (country_id) REFERENCES countries(id)
+    FOREIGN KEY (country_id) REFERENCES countries(id),
+    FOREIGN KEY (city_id) REFERENCES cities(id)
 ) ENGINE=InnoDB;
