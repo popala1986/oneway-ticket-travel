666201dcbfb260f92dfeffa8fbcac2d6bf29f8b1
Paweł Opala <p.opala1986@gmail.com>
2025-09-24 15:39:46 +0200

    feat: added custom OfferNotFoundException

---
+++ src/main/java/pl/onewaytickettravel/app/exception/OfferNotFoundException.java
@@ -0,0 +1,11 @@
+package pl.onewaytickettravel.app.exception;
+
+public class OfferNotFoundException extends RuntimeException{
+    /**
+     * Exception which is thrown when City object is not found in database
+     * @param message
+     */
+    public OfferNotFoundException(String message) {
+        super(message);
+    }
+}
