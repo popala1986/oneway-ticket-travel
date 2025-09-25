a2323626f77909763271c971b60f0c4b630f2344
Paweł Opala <p.opala1986@gmail.com>
2025-09-25 11:05:09 +0200

    chore: configure application-test.properties and enable test profile for Spring Boot tests

--- src/test/java/pl/onewaytickettravel/app/OnewayTicketTravelApplicationTests.java
+++ src/test/java/pl/onewaytickettravel/app/OnewayTicketTravelApplicationTests.java
@@ -2,7 +2,10 @@ package pl.onewaytickettravel.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class OnewayTicketTravelApplicationTests {
 
