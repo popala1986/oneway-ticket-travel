b616fee7307000926e9eeb93e6b80593bbde230e
Paweł Opala <p.opala1986@gmail.com>
2025-09-25 14:44:35 +0200

    docs: add Polish and English JavaDoc to OfferStatus enums

--- src/main/java/pl/onewaytickettravel/app/entities/OfferStatus.java
+++ src/main/java/pl/onewaytickettravel/app/entities/OfferStatus.java
@@ -1,7 +1,30 @@
 package pl.onewaytickettravel.app.entities;
 
+
+/**
+ * Enum reprezentujący status dostępności oferty turystycznej.
+ * Może przyjmować wartości: AVAILABLE, RESERVED, CANCELLED.
+ *
+ * Enum representing the availability status of a travel offer.
+ * Possible values: AVAILABLE, RESERVED, CANCELLED.
+ */
 public enum OfferStatus {
+
+    /**
+     * Oferta jest dostępna do rezerwacji.
+     * Offer is available for booking.
+     */
     AVAILABLE,
+
+    /**
+     * Oferta została zarezerwowana przez użytkownika.
+     * Offer has been reserved by a user.
+     */
     RESERVED,
+
+    /**
+     * Oferta została anulowana i nie jest dostępna.
+     * Offer has been cancelled and is no longer available.
+     */
     CANCELLED
 }
