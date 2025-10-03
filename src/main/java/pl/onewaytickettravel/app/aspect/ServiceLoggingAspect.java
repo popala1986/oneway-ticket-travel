d831af37ee8a43afda5f82f4364f152b48126e18
Paweł Opala <p.opala1986@gmail.com>
2025-10-03 19:33:31 +0200

    chore(aop): add @Order(1) annotation to ServiceLoggingAspect

--- src/main/java/pl/onewaytickettravel/app/aspect/ServiceLoggingAspect.java
+++ src/main/java/pl/onewaytickettravel/app/aspect/ServiceLoggingAspect.java
@@ -3,6 +3,7 @@ package pl.onewaytickettravel.app.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class ServiceLoggingAspect {
 
 
